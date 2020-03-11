9c25ef20dab9cddb736b061aad654f068b3dbaa8
Alzaaa <57305633+Alzaaa@users.noreply.github.com>
2020-03-11 19:32:38 +0800

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,8 @@
+public class tes {
+   public static void main(String[] args){
+        System.out.print("hellow word\n");
+        System.out.print("life is journey\n");
+        System.out.print("not bad");
+}
+}
+
